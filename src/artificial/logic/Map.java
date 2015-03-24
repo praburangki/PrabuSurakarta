ed6b35e0dcd22adb3cd2ba44deecbbfa9f600715
praburangki <praburangki@gmail.com>
2015-03-24 17:05:13 +0800

    Revert 1fa2f95..65e0864
    This rolls back to commit 1fa2f952ac80feefbf775ff72cb55c88fd29f2ce.

---
+++ src/artificial/logic/Map.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package artificial.logic;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author praburangki
+ */
+class Map {
+
+    public static final int MAXSIZE = 6;
+    public static final int NOSTONE = 0;
+    public static final int WHITE = 1;
+    public static final int BLACK = 2;
+    public static final int SELECT = 3;
+
+    private int whiteNum;
+    private int blackNum;
+
+    public int[][] map = new int[MAXSIZE][MAXSIZE];
+
+    public void clear() {
+        for (int i = 0; i < MAXSIZE; i++) {
+            Arrays.fill(map[i], 0);
+        }
+    }
+
+    public void init() {
+        clear();
+
+        whiteNum = blackNum = 12;
+    }
+
+    public boolean isInMap(int x, int y) {
+        return ((x >= 0) && (x < MAXSIZE)) && ((y >= 0) && (y < MAXSIZE));
+    }
+
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < map.length; i++) {
+            s += Arrays.toString(map[i]) + "\n";
+        }
+
+        return s;
+    }
+}
--- src/ch08/ai/AiPlayerHandler.java
+++ src/ch08/ai/AiPlayerHandler.java
@@ -47,96 +47,72 @@ public class AiPlayerHandler implements IPlayerHandler {
      */
     private Move getBestMove() {
         System.out.println("Getting best move");
-        ConsoleGui.printCurrentGameState(game);
         System.out.println("thingking...");
-        
-        List<Move> validMoves = generateMoves(false);
-        int bestResult = Integer.MIN_VALUE;
-        Move bestMove = null;
-        
-        for(Move move : validMoves) {
-            doMove(move);
-            
-            int evaluationResult = -1 * alphaBeta(this.maxDepth, Integer.MIN_VALUE, Integer.MAX_VALUE, 1);
-            undoMove(move);
-            if(evaluationResult > bestResult) {
-                bestResult = evaluationResult;
-                bestMove = move;
-            }
-        }
-        
-        System.out.println("done thinking! best move is " + bestMove);
-        
+
+        System.out.println("done thinking! best move is " + bestMove + "with bestResult : " + bestResult);
+
         return bestMove;
     }
-    
+
+    private String alphaBeta(int depth, int beta, int alpha, String move, int player) {
+        String list = possibleMoves();
+        if (depth == 0 || list.length() == 0) {
+            Evaluate eval = new Evaluate(game);
+            return move + (eval.evalue(game.getMap().map, player) * (player * 2 - 1));
+        }
+
+        player = 1 - player;
+
+        for (int i = 0; i < list.length(); i += 5) {
+            doMove(list.substring(i, i + 5));
+            flipBoard();
+            String returnString = alphaBeta(depth - 1, beta, alpha, list.substring(i, i + 5), player);
+            int value = Integer.valueOf(returnString.substring(5));
+            flipBoard();
+            undoMove(list.substring(i, i + 5));
+            if (player == 0) {
+                if (value <= beta) {
+                    beta = value;
+                    if (depth == maxDepth) {
+                        move = returnString.substring(0, 5);
+                    }
+                }
+            } else {
+                if (value > alpha) {
+                    alpha = value;
+                    if (depth == maxDepth) {
+                        move = returnString.substring(0, 5);
+                    }
+                }
+            }
+
+            if (alpha >= beta) {
+                if (player == 0) {
+                    return move + beta;
+                } else {
+                    return move + alpha;
+                }
+            }
+        }
+
+        if (player == 0) {
+            return move + beta;
+        } else {
+            return move + alpha;
+        }
+    }
+
     @Override
     public void moveSuccessfullyExecuted(Move move) {
         System.out.println("executed : " + move);
     }
 
-    private int alphaBeta(int depth, int alpha, int beta, int color) {
-        if(depth <= 0
-                || this.game.getGameState() == Game.GAME_STATE_END_WHITE_WON
-                || this.game.getGameState() == Game.GAME_STATE_END_BLACK_WON) {
-//            return evaluate.evalue(map.map, color);
-            return 0;
-        }
-        List<Move> moves = generateMoves(false);
-        for (Move move : moves) {
-            doMove(move);
-            int value = -1 * alphaBeta(depth - 1, -1 * beta, -1 * alpha, color ^ 3);
-            undoMove(move);
-            if(value > alpha) {
-                alpha = value;
-            }
-            if (value >= beta) break;
-        }
-        return alpha;
-    }
-    
-    private int evaluateState() {
-        
-        
-        return 0;
-    }
-    
     private void undoMove(Move move) {
         this.game.undoMove(move);
     }
-    
+
     private void doMove(Move move) {
         this.game.movePiece(move);
         this.game.changeGameState();
-    }
-    
-    private List<Move> generateMoves(boolean debug) {
-        List<Piece> pieces = this.game.getPieces();
-        List<Move> validMoves = new ArrayList<>();
-        Move testMove = new Move(0, 0, 0, 0);
-        
-        int pieceColor = this.game.getGameState() == Game.GAME_STATE_WHITE ? Piece.COLOR_WHITE : Piece.COLOR_BLACK;
-        
-        for(Piece piece : pieces) {
-            if(pieceColor == piece.getColor()) {
-                testMove.sourceRow = piece.getRow();
-                testMove.sourceColumn = piece.getColumn();
-                
-                for(int targetRow = Piece.ROW_1; targetRow <= Piece.ROW_6; targetRow++) {
-                    for(int targetColumn = Piece.COLUMN_A; targetColumn <= Piece.COLUMN_F; targetColumn++) {
-                        
-                        testMove.targetRow = targetRow;
-                        testMove.targetColumn = targetColumn;
-                        
-                        if(debug) System.out.println("testing move : " + testMove);
-                        
-                        if(this.validator.isMoveValid(testMove, false)) {
-                            validMoves.add(testMove.copy());
-                        }
-                    }
-                }
-            }
-        }
-        return validMoves;
     }
 }
