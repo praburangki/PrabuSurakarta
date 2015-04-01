fce46bb91aced2395d777793507a6c5d28edbf03
praburangki <praburangki@gmail.com>
2015-04-01 17:51:22 +0700

    temp

--- src/ch08/logic/MoveValidator3.java
+++ src/ch08/logic/MoveValidator3.java
@@ -5,7 +5,6 @@
  */
 package ch08.logic;
 
-import ch08.console.ConsoleGui;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
@@ -14,14 +13,14 @@ import java.util.Stack;
  *
  * @author praburangki
  */
-public class MoveValidator2 {
+public class MoveValidator3 {
 
     private Game game;
     private Piece sourcePiece, targetPiece;
     private boolean debug;
     private Map map;
 
-    public MoveValidator2(Game game, Map map) {
+    public MoveValidator3(Game game, Map map) {
         this.game = game;
         this.map = map;
     }
